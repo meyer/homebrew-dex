02fe9ecb1c45be0b2377658467e95bcefdd8764a
Mike Meyer <github.com@meyer.fm>
2016-06-09 03:07:40 -0700

    Add plist method to formula

--- Formula/dexd.rb
+++ Formula/dexd.rb
@@ -7,17 +7,48 @@ class Dexd < Formula
     bin.install "build/dexd"
   end
 
+  plist_options :manual => "dexd"
+
+  def plist
+    <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+
+        <key>KeepAlive</key>
+        <true/>
+
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/dexd</string>
+          <string>-cert_dir</string>
+          <string>#{var}/dexd</string>
+        </array>
+
+        <key>StandardOutPath</key>
+        <string>#{var}/dexd/stdout.log</string>
+
+        <key>StandardErrorPath</key>
+        <string>#{var}/dexd/stderr.log</string>
+
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
+
   def caveats
     <<-EOS.undent
       Create or symlink a directory in which dex modules will be stored:
         mkdir -p ~/Dropbox/dexfiles
         ln -s ~/Dropbox/dexfiles ~/.dex
-
-      Write dexd launchagent to ~/Library/LaunchAgents:
-        dexd -install
-
-      Set dexd to start on boot:
-        launchctl load -w ~/Library/LaunchAgents/fm.meyer.dex.plist
     EOS
   end
 end
