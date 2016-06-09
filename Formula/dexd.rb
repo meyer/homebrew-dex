3d23ac1b6e39ec790fb9670df6a7846d89a06932
Mike Meyer <github.com@meyer.fm>
2016-06-09 12:18:29 -0700

    Wax eloquent in caveats

--- Formula/dexd.rb
+++ Formula/dexd.rb
@@ -49,6 +49,15 @@ class Dexd < Formula
       Create or symlink a directory in which dex modules will be stored:
         mkdir -p ~/Dropbox/dexfiles
         ln -s ~/Dropbox/dexfiles ~/.dex
+
+      dexd won’t start unless a folder or valid symlink is found at ~/.dex
+
+      Running dexd as a service is The Best Way, but if you want to do things
+      yourself, you’ll need to provide a cert_dir parameter. A self-signed
+      SSL certificate will be generated on first run and saved to cert_dir.
+
+      I could go on, but you’d probably have a nicer time reading the README:
+      https://github.com/meyer/dex/blob/master/README.md
     EOS
   end
 end
